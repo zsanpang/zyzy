6d0d36d31f05ea852593004401dcd6a0e894eac9
zsanpang <zsanpang@gmail.com>
2018-05-31 20:54:09 +0800

    第一次

---
+++ js/main.js
@@ -0,0 +1,266 @@
+$(function() {
+	$("#container").fullpage({
+		navigation: true, //是否显示项目导航
+		slidesNavigation: true, //是否显示左右滑块的项目导航
+		scrollingSpeed: 1000,
+		navigationColor: "#fff",
+		menu: '#menu',
+		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
+		afterLoad: function(anchorLink, index) {
+			if (index == 2) {
+				$(".section2 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section2 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+			if (index == 3) {
+				$(".section3 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section3 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+			if (index == 4) {
+				$(".section4 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section4 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+			if (index == 5) {
+				$(".section5 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section5 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+			if (index == 6) {
+				$(".section6 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section6 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+			if (index == 7) {
+				$(".section7 .tit .TitleEn h2").delay(0).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleEn font").delay(300).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleEn small").delay(600).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleEn span").delay(900).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleCh font").delay(1200).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleCh p").delay(1500).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+				$(".section7 .tit .TitleCh span").delay(1800).animate({
+					top: "0px",
+					opacity: "1"
+				}, 500);
+
+			}
+
+		},
+		//		afterRender: function() {
+		//			$(".tit .TitleEn h2").delay(0).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//			$(".tit .TitleEn font").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//			$(".tit .TitleEn small").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//			$(".tit .TitleEn span").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//			$(".tit .TitleCh font").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			},0);
+		//			$(".tit .TitleCh p").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//			$(".tit .TitleCh span").delay(00).animate({
+		//				top: "0px",
+		//				opacity: "1"
+		//			}, 0);
+		//
+		//		},
+		onLeave: function() {
+			$(".tit .TitleEn h2").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleEn font").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleEn small").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleEn span").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleCh font").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleCh p").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+			$(".tit .TitleCh span").delay(0).animate({
+				top: "-20px",
+				opacity: "0"
+			}, 1);
+
+		}
+		
+
+	});
+	setInterval(function() {
+			$.fn.fullpage.moveSlideRight();
+		}, 5000);
+});
